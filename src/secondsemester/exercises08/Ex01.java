27c313cb2003838abe9f21e860a51ae6dbd9c4a9
Lucas Lino <155203643+llucashenrique@users.noreply.github.com>
2024-08-24 15:10:08 -0300

    Inicial commit

---
+++ src/secondsemester/exercises08/Ex01.java
@@ -0,0 +1,95 @@
+package secondsemester.exercises08;
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int funcionariosAcimaDe7000 = 0;
+        int funcionariosAbaixoDe7000 = 0;
+        double somaSalarioLiquido = 0;
+        int contadorFuncionarios = 0;
+
+        System.out.print("Digite a quantidade de funcionários: ");
+        int quantidadeFuncionarios = scanner.nextInt();
+
+        for (int i = 0; i < quantidadeFuncionarios; i++) {
+            System.out.println("Funcionário " + (i + 1));
+
+            System.out.print("Código de matrícula: ");
+            int codigoMatricula = scanner.nextInt();
+
+            System.out.print("Salário Bruto: ");
+            double salarioBruto = scanner.nextDouble();
+
+            System.out.print("Número de dependentes: ");
+            int numeroDependentes = scanner.nextInt();
+
+
+            double salarioFamilia = numeroDependentes * 120.00;
+
+
+            double descontoINSS = calcularINSS(salarioBruto);
+
+            double salarioBaseIR = salarioBruto - descontoINSS - salarioFamilia;
+            double impostoRenda = calcularImpostoRenda(salarioBaseIR);
+
+
+            double salarioLiquido = salarioBruto + salarioFamilia - descontoINSS - impostoRenda;
+
+            contadorFuncionarios++;
+            somaSalarioLiquido += salarioLiquido;
+
+
+            if (salarioLiquido > 7000) {
+                funcionariosAcimaDe7000++;
+            } else {
+                funcionariosAbaixoDe7000++;
+            }
+
+            System.out.println("Código: " + codigoMatricula);
+            System.out.println("Salário Bruto: " + salarioBruto);
+            System.out.println("Salário Família: " + salarioFamilia);
+            System.out.println("Desconto INSS: " + descontoINSS);
+            System.out.println("Imposto de Renda: " + impostoRenda);
+            System.out.println("Salário Líquido: " + salarioLiquido);
+            System.out.println("--------------------------------------------");
+
+            if (contadorFuncionarios > 1) {
+                double mediaSalarioLiquido = somaSalarioLiquido / contadorFuncionarios;
+                System.out.println("Média dos salários líquidos até agora: " + mediaSalarioLiquido);
+            }
+        }
+
+        System.out.println("Total de funcionários com salário acima de 7000: " + funcionariosAcimaDe7000);
+        System.out.println("Total de funcionários com salário abaixo de 7000: " + funcionariosAbaixoDe7000);
+
+    }
+
+    public static double calcularINSS(double salarioBruto) {
+        if (salarioBruto <= 1100) {
+            return salarioBruto * 0.075;
+        } else if (salarioBruto <= 2200) {
+            return salarioBruto * 0.09;
+        } else if (salarioBruto <= 3300) {
+            return salarioBruto * 0.12;
+        } else {
+            return salarioBruto * 0.14;
+        }
+    }
+
+    public static double calcularImpostoRenda(double salarioBaseIR) {
+        if (salarioBaseIR <= 1903.98) {
+            return 0;
+        } else if (salarioBaseIR <= 2826.65) {
+            return salarioBaseIR * 0.075;
+        } else if (salarioBaseIR <= 3751.05) {
+            return salarioBaseIR * 0.15;
+        } else if (salarioBaseIR <= 4664.68) {
+            return salarioBaseIR * 0.225;
+        } else {
+            return salarioBaseIR * 0.275;
+        }
+    }
+}
---
+++ src/secondsemester/exercises08/Ex02.java
@@ -0,0 +1,87 @@
+package secondsemester.exercises08;
+
+import java.util.Scanner;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+
+        Employee[] employees = new Employee[4];
+
+        for (int i = 0; i < employees.length; i++) {
+            System.out.println("Funcionário " + (i + 1) + ":");
+
+            Employee employee = new Employee();
+
+            System.out.print("Digite o nome: ");
+            String name = input.nextLine();
+            employee.setName(name);
+
+            System.out.print("Digite a idade: ");
+            int age = input.nextInt();
+            input.nextLine();
+            employee.setAge(age);
+
+            System.out.print("Em qual estado você vive? ");
+            String address = input.nextLine();
+            employee.setAddress(address);
+
+
+            employees[i] = employee;
+        }
+
+        int countInState = 0;
+        int countOutOfState = 0;
+        int countAbove60 = 0;
+
+        for (Employee employee : employees) {
+            if (employee.getAddress().equalsIgnoreCase("SP")) {
+                countInState++;
+            } else {
+                countOutOfState++;
+            }
+
+            if (employee.getAge() >= 60) {
+                countAbove60++;
+            }
+        }
+
+        System.out.println("Número de motoristas que residem no estado (SP): " + countInState);
+        System.out.println("Número de motoristas que residem fora do estado (SP): " + countOutOfState);
+        System.out.println("Número de motoristas com idade acima de 60 anos: " + countAbove60);
+
+        input.close();
+    }
+
+    static class Employee {
+        private String name;
+        private int age;
+        private String address;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+    }
+}
+
